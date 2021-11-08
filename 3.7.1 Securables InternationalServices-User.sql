996a5deaad84fff478fa0736e3bcc231e322c4bc
Jerry D Farrell <14806267+jfarrell423@users.noreply.github.com>
2021-11-08 08:12:49 -0500

    Adding the securables script.

---
+++ 3.7.1 Securables InternationalServices-User.sql
@@ -0,0 +1,476 @@
+USE [InternationalServices]
+
+
+PRINT N'Altering permissions on  [dbo].[fnFormatDate]'
+GO
+GRANT EXECUTE ON  [dbo].[fnFormatDate] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[fnGetEFormFieldValue]'
+GO
+GRANT EXECUTE ON  [dbo].[fnGetEFormFieldValue] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[fnTransformAddress]'
+GO
+GRANT SELECT ON  [dbo].[fnTransformAddress] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[fnTransformSEVISPassportName]'
+GO
+GRANT EXECUTE ON  [dbo].[fnTransformSEVISPassportName] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[fnTransformSEVISPreferredName]'
+GO
+GRANT EXECUTE ON  [dbo].[fnTransformSEVISPreferredName] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[fnTransformSEVISPrincipalName]'
+GO
+GRANT EXECUTE ON  [dbo].[fnTransformSEVISPrincipalName] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[fnTrunc]'
+GO
+GRANT EXECUTE ON  [dbo].[fnTrunc] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on TYPE:: [dbo].[jbAddressTable]'
+GO
+GRANT EXECUTE ON TYPE:: [dbo].[jbAddressTable] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on TYPE:: [dbo].[jbEFormTable]'
+GO
+GRANT EXECUTE ON TYPE:: [dbo].[jbEFormTable] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE001]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE001] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE002]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE002] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE003]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE003] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE004]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE004] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE005]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE005] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE006]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE006] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE007]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE007] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE008]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE008] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE009]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE009] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE010]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE010] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE011]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE011] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE012]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE012] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE013]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE013] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE014]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE014] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE015]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE015] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE016]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE016] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE017]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE017] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE018]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE018] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE019]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE019] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE020]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE020] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE021]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE021] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE022]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE022] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE023]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE023] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE024]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE024] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE025]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE025] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE026]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE026] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE027]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE027] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE028]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE028] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE029]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE029] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE030]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE030] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE031]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE031] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE032]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE032] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE033]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE033] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE034]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE034] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE035]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE035] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE036]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE036] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE037]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE037] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE038]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE038] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE039]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE039] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistLetterExtensionLE040]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistLetterExtensionLE040] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT001]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT001] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT022]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT022] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT023]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT023] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT024]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT024] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT025]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT025] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT026]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT026] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT028]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT028] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT029]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT029] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT031]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT031] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT032]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT032] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT038]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT038] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT039]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT039] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT040]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT040] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT041]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT041] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT042]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT042] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT043]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT043] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT044]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT044] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT045]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT045] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT046]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT046] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT047]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT047] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT048]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT048] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT049]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT049] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[checklistTaskExtensionT050]'
+GO
+GRANT EXECUTE ON  [dbo].[checklistTaskExtensionT050] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[dataFeedCoreClient]'
+GO
+GRANT EXECUTE ON  [dbo].[dataFeedCoreClient] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[dataFeedReindex]'
+GO
+GRANT EXECUTE ON  [dbo].[dataFeedReindex] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[spChecklistDepartmentReport]'
+GO
+GRANT EXECUTE ON  [dbo].[spChecklistDepartmentReport] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[spCreateEformGroupChecklist]'
+GO
+GRANT EXECUTE ON  [dbo].[spCreateEformGroupChecklist] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[spCreateEformGroup]'
+GO
+GRANT EXECUTE ON  [dbo].[spCreateEformGroup] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[spGeneratePIN]'
+GO
+GRANT EXECUTE ON  [dbo].[spGeneratePIN] TO [InternationalServices-User]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+PRINT N'Altering permissions on  [dbo].[spTransformIndividualAddress]'
+GO
+GRANT EXECUTE ON  [dbo].[spTransformIndividualAddress] TO [InternationalServices-User]
+GO
+PRINT N'The securables execute permissions have been successfully applied to InternationalServices-User'
